604ff580ea05e4fd7f867f36d04cf767bddd2b4b
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-23 00:16:18 +0300

    Remove unnecessary endpoint mappings
    Removed the '/hey' and '/whois' endpoint mappings in the `Program.cs` of the Wowshop API. These mappings were unnecessary and not part of the

--- Wowshop.API/Program.cs
+++ Wowshop.API/Program.cs
@@ -2,7 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/hey", () => "hey");
-app.MapGet("/whois", () => "me");
 
 app.Run();
