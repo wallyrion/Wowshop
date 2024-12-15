bba138d857fc65ac7de76a0625b7c243744f6b33
Oleksii KorniienKo <kornienko1296@gmail.com>
2024-12-15 18:40:42 +0100

    Confirm deployment step

--- Wowshop.API/Program.cs
+++ Wowshop.API/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    Console.WriteLine($"Hello World!. Time: {DateTime.Now}");
+    
+    return "Hello World!";
+});
 app.MapGet("goodbye", () => "Goodbye");
+
+app.MapGet("/health", (IConfiguration configuration) => new
+{
+    ImageTag = configuration["CurrentImageTag"],
+});
 
 app.Run();
